e8e575f6c7d3e5431bfa07629a24bc44ad136193
Vaniah Christian Mukisa <mukisa.vaniah@students.mak.ac.ug>
2025-04-01 10:56:11 +0300

    heehe

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -1,9 +1,21 @@
+"use client"
+
 import type React from "react"
+import { ThemeProvider } from "@/components/theme-provider"
+import "@/app/globals.css"
 
 export default function MarketingLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
-  return children
+  return (
+    <html lang="en">
+      <body>
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  )
 }
