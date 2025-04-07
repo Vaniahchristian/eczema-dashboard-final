eaa32ca565f772e8b22c3b58eaaf51b8d2db2ca2
Vaniah Christian Mukisa <mukisa.vaniah@students.mak.ac.ug>
2025-04-07 18:54:39 +0300

    support channel

--- components/support/support-page.tsx
+++ components/support/support-page.tsx
@@ -25,7 +25,7 @@ export default function SupportPage() {
           <div className="grid grid-cols-1 lg:grid-cols-3 gap-8 mt-8">
             <div className="lg:col-span-2 space-y-8">
               <SupportAssistant />
-              <SupportChannels />
+              {/* <SupportChannels /> */}
               <SupportTicket />
               <FeedbackSection />
             </div>
