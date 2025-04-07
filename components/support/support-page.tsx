c32f726ec6008808dde41a184b43f30bff79a17f
Vaniah Christian Mukisa <mukisa.vaniah@students.mak.ac.ug>
2025-04-07 19:29:08 +0300

    center

--- components/support/support-page.tsx
+++ components/support/support-page.tsx
@@ -13,7 +13,7 @@ import FeedbackSection from "@/components/support/feedback-section"
 export default function SupportPage() {
   return (
     <DashboardLayout>
-      <div className="p-6 md:p-8 mx-auto max-w-screen-2xl w-full">
+      <div className="p-6 md:p-8 mx-auto max-w-screen-2xl w-full">  
         <motion.div
           initial={{ opacity: 0, y: 20 }}
           animate={{ opacity: 1, y: 0 }}
